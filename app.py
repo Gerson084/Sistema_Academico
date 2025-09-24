d3898a251c9193874379c8467ce50657ea9bea8a
Eunice G <katherine.gutierrez@catolica.edu.sv>
2025-09-24 00:00:31 -0600

    Guardando cambios en app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, render_template
 from db import DatabaseConfig, test_connection
 from appEstudiantes import estudiantes_bp
+
 from routes.user_route import users
+from routes.rol_route import rol
 from models import Estudiantes
 from dotenv import load_dotenv
 import os
@@ -17,13 +19,19 @@ app.secret_key = os.getenv("SECRET_KEY")  # <--- esto es crucial
 # Inicializar la DB
 db = DatabaseConfig.init_app(app)
 
-# Registrar blueprints
+# Registrar blueprint
 app.register_blueprint(estudiantes_bp, url_prefix="/estudiantes")
 app.register_blueprint(users)
+app.register_blueprint(rol)
+
+
+
 
 @app.route('/')
 def home():
     return render_template('base.html')
+
+
 
 @app.route('/test-db')
 def test_db():
@@ -48,10 +56,8 @@ if __name__ == '__main__':
         else:
             print("❌ La tabla 'estudiantes' no se encuentra en la base de datos")
 
-        # Crear tablas según modelos
+        # Asegurarse de que SQLAlchemy vea los modelos
         db.create_all()
-
         # Probar conexión y ver tablas
         print(test_connection())
-
     app.run(debug=True)
